61929b25844771dfdf4b8410e09ab4e80bc77351
David Möller <davidm@student.chalmers.se>
2020-07-29 09:54:49 +0200

    Created a frontend graph which shows smhi forecast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import {
+  LineChart,
+  XAxis,
+  Tooltip,
+  CartesianGrid,
+  Line,
+  YAxis,
+  Legend,
+} from "recharts";
 
-function App() {
-  return <div>Hello</div>;
-}
+//const data = [{ val: { hej: 1 } }, { val: { hej: 2 } }, { val: { hej: 2.5 } }];
+const getData = (timeSeries) =>
+  timeSeries.map((e) => e.parameters.find((param) => param.name === "t"));
+
+const App = () => {
+  const [data, setData] = useState();
+  useEffect(() => {
+    Axios.get("/api/smhi").then((res) => {
+      setData(getData(res.data.timeSeries));
+      console.log(res.data);
+    });
+  }, []);
+  return (
+    <LineChart
+      width={1300}
+      height={500}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+
+      <Line type="monotone" dataKey="values.0" name="Value" stroke="#82ca9d" />
+    </LineChart>
+  );
+};
 
 export default App;
